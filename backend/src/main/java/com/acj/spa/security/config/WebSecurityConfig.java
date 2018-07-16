1fc2e249bc8b1b458156e67e7f55751009ab64d9
unknown <jeanoliveiragomes1@hotmail.com>
2018-07-16 15:29:26 -0300

    Permitindo Cross Origin *, adicionando SpringSecurity permitindo * e sem perfil.

---
+++ backend/src/main/java/com/acj/spa/security/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.acj.spa.security.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers(HttpMethod.PUT,"/api/*").permitAll();	
+		http.authorizeRequests().antMatchers(HttpMethod.POST,"/api/*").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"api/*").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE,"api/*").permitAll();
+		http.cors().configure(http);
+		http.csrf().disable();
+	
+	}
+	
+	
+		//Allow CORS
+		@Bean
+		CorsConfigurationSource corsConfigurationSource() {
+			CorsConfiguration configuration = new CorsConfiguration();
+			configuration.setAllowedOrigins(Arrays.asList("*"));
+			configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+			configuration.setAllowedHeaders(Arrays.asList("*"));
+			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+			source.registerCorsConfiguration("/**", configuration);
+			return source;
+		}
+}
